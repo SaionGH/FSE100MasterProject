19a8479f01a6c8e1d587d4f6787eea005d4d17d6
BKSynigal <bsynigal@asu.edu>
2024-11-11 21:53:33 -0700

    updated the main loop to be based on screens

--- Beonce's Code/GamePlayLoopController.js
+++ Beonce's Code/GamePlayLoopController.js
@@ -1,10 +1,20 @@
 // Main Sketch File
 
 // Declare global variables
-//let level;
+let level = null;
 let player;
 //let orderManager;
 //let gameUI;
+let buttons = [];
+let gameScreen = 0;
+let map1 = [["Counter", "Counter", "Trash", "Counter", "CuttingBoard", "Counter", "Counter", "Counter", "Grill", "Grill"], ["Counter", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"], ["Lettuce", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"], ["MeatPatty", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"], ["Cheese", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"], ["Bun", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"], ["Counter", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"], ["Plates", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"], ["Delivery", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"], ["Delivery", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"]]
+let map2 = [["Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"], ["Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "CuttingBoard", "Counter", "Grill", "Grill"], ["Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"], ["Floor", "Floor", "Counter", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"], ["Floor", "Floor", "Trash", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"], ["Floor", "Floor", "Counter", "Floor", "Floor", "Floor", "MeatPatty", "Lettuce", "Cheese", "Bun"], ["Floor", "Floor", "Drink", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"], ["Floor", "Floor", "Cups", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"], ["Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"], ["Floor", "Floor", "Floor", "Counter", "Counter", "Plates", "Delivery", "Delivery", "Floor", "Floor"]]
+let map3 = [["Counter", "Lettuce", "Bun", "Counter", "Counter", "CuttingBoard", "Counter", "Counter", "Grill", "Grill"], ["Counter", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"], ["Cheese", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"], ["MeatPatty", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"], ["Counter", "Counter", "Counter", "Trash", "Counter", "Floor", "Floor", "Floor", "Floor", "Floor"], ["Floor", "Floor", "Floor", "Floor", "Drink", "Floor", "Floor", "Salmon", "Seaweed", "Rice"], ["Floor", "Floor", "Floor", "Floor", "Cups", "Floor", "Floor", "Floor", "Floor", "Floor"], ["Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"], ["Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor"], ["Counter", "Counter", "Delivery", "Delivery", "Plates", "Floor", "Floor", "CuttingBoard", "Counter", "Counter"]]
+let map4 = [["Counter", "Counter", "Counter", "Counter", "Delivery", "Delivery", "Counter", "Counter", "Counter", "Counter"], ["Counter", "Counter", "Counter", "Plates", "Floor", "Floor", "Plates", "Counter", "Counter", "Counter"], ["Drink", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Counter"], ["Counter", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "CuttingBoard"], ["Cups", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Counter"], ["Counter", "Floor", "Floor", "Lettuce", "Floor", "Floor", "Salmon", "Floor", "Floor", "Counter"], ["Grill", "Floor", "Floor", "MeatPatty", "Floor", "Floor", "Seaweed", "Floor", "Floor", "Trash"], ["Grill", "Floor", "Floor", "Cheese", "Floor", "Floor", "Rice", "Floor", "Floor", "Counter"], ["Counter", "Floor", "Floor", "Bun", "Floor", "Floor", "Cucumber", "Floor", "Floor", "Counter"], ["Counter", "CuttingBoard", "Counter", "Counter", "Counter", "Counter", "Counter", "Counter", "Counter", "Counter"]]
+let map5 = [["Counter", "Counter", "MeatPatty", "Lettuce", "Counter", "Counter", "Counter", "Counter", "Counter", "Counter"], ["Grill", "Floor", "Floor", "Floor", "Cheese", "Cucumber", "Floor", "Floor", "Floor", "CuttingBoard"], ["Grill", "Floor", "Floor", "Floor", "Bun", "Salmon", "Floor", "Floor", "Floor", "Counter"], ["Counter", "Floor", "Floor", "Floor", "Drink", "Cups", "Floor", "Floor", "Floor", "Plates"], ["Counter", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Delivery"], ["CuttingBoard", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Delivery"], ["Counter", "Floor", "Floor", "Floor", "Noodles", "Rice", "Floor", "Floor", "Floor", "Plates"], ["Grill", "Floor", "Floor", "Floor", "Tomato", "Seaweed", "Floor", "Floor", "Floor", "Counter"], ["Grill", "Floor", "Floor", "Floor", "Counter", "Counter", "Floor", "Floor", "Floor", "CuttingBoard"], ["Counter", "Counter", "Trash", "Counter", "Counter", "Counter", "Counter", "Trash", "Counter", "Counter"]]
+let map6 = [["Counter", "Counter", "Noodles", "Tomato", "MeatBalls", "Counter", "Counter", "Counter", "Counter", "Counter"], ["Grill", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Counter"], ["Grill", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "CuttingBoard"], ["Counter", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Counter"], ["Cups", "Floor", "Floor", "Cheese", "Lettuce", "Seaweed", "Salmon", "Floor", "Floor", "Counter"], ["Drink", "Floor", "Floor", "Bun", "MeatPatty", "Cucumber", "Rice", "Floor", "Floor", "Counter"], ["Counter", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "CuttingBoard"], ["Grill", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Counter"], ["Grill", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Floor", "Counter"], ["Floor", "Floor", "Floor", "Plates", "Delivery", "Delivery", "Plates", "Floor", "Floor", "Floor"]]
+
+
 
 function preload() {
   // Load images, sprites, and other assets if needed
@@ -13,7 +23,12 @@ function preload() {
 
 function setup() {
   createCanvas(600, 600); // Set the canvas size
-
+  buttons.push(new Button(50, 50, 120, 50, "Button 1", () => { gameScreen = 1; }));
+  buttons.push(new Button(50, 120, 120, 50, "Button 2", () => { gameScreen = 2; }));
+  buttons.push(new Button(50, 190, 120, 50, "Button 3", () => { gameScreen = 3; }));
+  buttons.push(new Button(50, 260, 120, 50, "Button 4", () => { gameScreen = 4; }));
+  buttons.push(new Button(50, 330, 120, 50, "Button 5", () => { gameScreen = 5; }));
+  buttons.push(new Button(50, 400, 120, 50, "Button 6", () => { gameScreen = 6; }));
   // Initialize the level
   // MVP 2: Build the Level class and initialize it here
   let mapData = [
@@ -26,17 +41,10 @@ function setup() {
     ['C', 'C', 'C', 'C', 'C', 'C', 'C', 'C'],
     ['C', 'C', 'C', 'C', 'C', 'C', 'C', 'C'],
   ];
-let map1 = [["Counter","Counter","Trash","Counter","CuttingBoard","Counter","Counter","Counter","Grill","Grill"],["Counter","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor"],["Lettuce","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor"],["MeatPatty","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor"],["Cheese","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor"],["Bun","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor"],["Counter","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor"],["Plates","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor"],["Delivery","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor"],["Delivery","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor"]]
-let map2 = [["Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor"],["Floor","Floor","Floor","Floor","Floor","Floor","CuttingBoard","Counter","Grill","Grill"],["Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor"],["Floor","Floor","Counter","Floor","Floor","Floor","Floor","Floor","Floor","Floor"],["Floor","Floor","Trash","Floor","Floor","Floor","Floor","Floor","Floor","Floor"],["Floor","Floor","Counter","Floor","Floor","Floor","MeatPatty","Lettuce","Cheese","Bun"],["Floor","Floor","Drink","Floor","Floor","Floor","Floor","Floor","Floor","Floor"],["Floor","Floor","Cups","Floor","Floor","Floor","Floor","Floor","Floor","Floor"],["Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor"],["Floor","Floor","Floor","Counter","Counter","Plates","Delivery","Delivery","Floor","Floor"]]
-let map3 =[["Counter","Lettuce","Bun","Counter","Counter","CuttingBoard","Counter","Counter","Grill","Grill"],["Counter","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor"],["Cheese","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor"],["MeatPatty","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor"],["Counter","Counter","Counter","Trash","Counter","Floor","Floor","Floor","Floor","Floor"],["Floor","Floor","Floor","Floor","Drink","Floor","Floor","Salmon","Seaweed","Rice"],["Floor","Floor","Floor","Floor","Cups","Floor","Floor","Floor","Floor","Floor"],["Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor"],["Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor"],["Counter","Counter","Delivery","Delivery","Plates","Floor","Floor","CuttingBoard","Counter","Counter"]]
-let map4 =[["Counter","Counter","Counter","Counter","Delivery","Delivery","Counter","Counter","Counter","Counter"],["Counter","Counter","Counter","Plates","Floor","Floor","Plates","Counter","Counter","Counter"],["Drink","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Counter"],["Counter","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","CuttingBoard"],["Cups","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Counter"],["Counter","Floor","Floor","Lettuce","Floor","Floor","Salmon","Floor","Floor","Counter"],["Grill","Floor","Floor","MeatPatty","Floor","Floor","Seaweed","Floor","Floor","Trash"],["Grill","Floor","Floor","Cheese","Floor","Floor","Rice","Floor","Floor","Counter"],["Counter","Floor","Floor","Bun","Floor","Floor","Cucumber","Floor","Floor","Counter"],["Counter","CuttingBoard","Counter","Counter","Counter","Counter","Counter","Counter","Counter","Counter"]]
-let map5 = [["Counter","Counter","MeatPatty","Lettuce","Counter","Counter","Counter","Counter","Counter","Counter"],["Grill","Floor","Floor","Floor","Cheese","Cucumber","Floor","Floor","Floor","CuttingBoard"],["Grill","Floor","Floor","Floor","Bun","Salmon","Floor","Floor","Floor","Counter"],["Counter","Floor","Floor","Floor","Drink","Cups","Floor","Floor","Floor","Plates"],["Counter","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Delivery"],["CuttingBoard","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Delivery"],["Counter","Floor","Floor","Floor","Noodles","Rice","Floor","Floor","Floor","Plates"],["Grill","Floor","Floor","Floor","Tomato","Seaweed","Floor","Floor","Floor","Counter"],["Grill","Floor","Floor","Floor","Counter","Counter","Floor","Floor","Floor","CuttingBoard"],["Counter","Counter","Trash","Counter","Counter","Counter","Counter","Trash","Counter","Counter"]]
-let map6 =[["Counter","Counter","Noodles","Tomato","MeatBalls","Counter","Counter","Counter","Counter","Counter"],["Grill","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Counter"],["Grill","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","CuttingBoard"],["Counter","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Counter"],["Cups","Floor","Floor","Cheese","Lettuce","Seaweed","Salmon","Floor","Floor","Counter"],["Drink","Floor","Floor","Bun","MeatPatty","Cucumber","Rice","Floor","Floor","Counter"],["Counter","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","CuttingBoard"],["Grill","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Counter"],["Grill","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Floor","Counter"],["Floor","Floor","Floor","Plates","Delivery","Delivery","Plates","Floor","Floor","Floor"]]
-  level = new Level(map6);
 
   // Initialize the player
   // MVP 1: Build the PlayerController class and initialize it here
-  player = new PlayerController(width/2, height/2);
+  player = new PlayerController(width / 2, height / 2);
 
   // Initialize the order manager
   // MVP 4: Build the OrderManager class and initialize it here
@@ -47,7 +55,7 @@ let map6 =[["Counter","Counter","Noodles","Tomato","MeatBalls","Counter","Counte
   //gameUI = new GameUI();
 }
 
-function draw() {
+function gamePlay() {
   background(200); // Clear the background
 
   // Draw the level
@@ -67,4 +75,74 @@ function draw() {
   //// Draw the game UI
   //// MVP 5: Ensure GameUI's drawUI method works
   //gameUI.drawUI();
+}
+
+function menuScreen() {
+  background(220);
+
+  // Draw all buttons
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].display();
+  }
+}
+
+function draw() {
+  switch (gameScreen) {
+    case 0:
+      level = null;
+      menuScreen()
+      break;
+    case 1:
+      if (level === null) {
+
+        level = new Level(map1);
+      } else {
+        gamePlay()
+      }
+      break;
+    case 2:
+      if (level === null) {
+
+        level = new Level(map2);
+      } else {
+        gamePlay()
+      }
+      break;
+    case 3:
+      if (level === null) {
+
+        level = new Level(map3);
+      } else {
+        gamePlay()
+      }
+      break;
+    case 4:
+        if (level === null) {
+  
+          level = new Level(map4);
+        } else {
+          gamePlay()
+        }
+        break;
+    case 5:
+          if (level === null) {
+    
+            level = new Level(map5);
+          } else {
+            gamePlay()
+          }
+          break;
+      case 6:
+            if (level === null) {
+      
+              level = new Level(map6);
+            } else {
+              gamePlay()
+            }
+            break;            
+    default:
+      menuScreen()
+      break;  
+
+  }
 }
