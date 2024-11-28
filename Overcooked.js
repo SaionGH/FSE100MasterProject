aa18c37c43b21df271197b9c4e95e9fac1ab3a4c
ANVITAMEKA884 <ameka3@asu.edu>
2024-11-27 21:48:39 -0700

    Update Overcooked.js

--- Overcooked.js
+++ Overcooked.js
@@ -119,7 +119,6 @@ function draw() {
   } else if (page === 8) {
     level.update();
     level.display();
-    displayTimer();
   }
   // Count down the timer if the game is in progress
     if (gameStarted && page === 8) {
