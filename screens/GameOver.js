173b8c865ad7d26d27bc5799ea502bfcae3b3441
ANVITAMEKA884 <ameka3@asu.edu>
2024-11-27 22:52:14 -0700

    Update GameOver.js

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -8,6 +8,5 @@ function displayGameOver() {
   text("Over!", width / 2, height / 2 - 74);
   textSize(30);
   text("Your Score: " + playerScore, width / 2, height / 2 + 20);
-  dashboardButton.show();
   gameOver = true;
 }
