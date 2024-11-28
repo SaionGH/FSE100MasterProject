e8d9cd7ddfaa6ae3ae3667530281307cee9f3846
ANVITAMEKA884 <ameka3@asu.edu>
2024-11-27 23:10:27 -0700

    Update Overcooked.js

--- Overcooked.js
+++ Overcooked.js
@@ -33,11 +33,6 @@ function loadData() {
   // Load selected level
   selectedLevel = parseInt(localStorage.getItem("selectedLevel")) || 0;
 }
-function dashboard() {
-  page = 7; // Navigate to the scoreboard page
-  dashboardButton.hide(); // Hide the dashboard button
-  console.log("Navigated to Dashboard (Page 7)");
-}
 function clearData() {
   // Clear all data from localStorage
   localStorage.removeItem("scoreboard");
@@ -65,15 +60,7 @@ function handleClearData() {
 }
 function setup() {
   createCanvas(600, 600);
-  dashboardButton = createButton("Click to View the Leaderboard");
-  dashboardButton.position(width / 2 - 150, height / 2 + 120); // Position the button under the score
-  dashboardButton.style("background-color", "black");
-  dashboardButton.style("color", "white");
-  dashboardButton.style("font-size", "20px");
-  dashboardButton.style("padding", "10px");
-  dashboardButton.style("border-radius", "5px");
-  dashboardButton.hide(); // Initially hide the button
-  dashboardButton.mousePressed(dashboard);
+
   let clearDataButton = createButton("Clear Data");
   clearDataButton.position(490, 10); // Adjust the position as needed
   // Style the button to make it red
