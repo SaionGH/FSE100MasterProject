f7fdb891f1a23dbe42a79bfd9062785e00352c98
ANVITAMEKA884 <ameka3@asu.edu>
2024-11-27 23:30:17 -0700

    Update Overcooked.js

--- Overcooked.js
+++ Overcooked.js
@@ -33,11 +33,7 @@ function loadData() {
   // Load selected level
   selectedLevel = parseInt(localStorage.getItem("selectedLevel")) || 0;
 }
-function dashboard() {
-  page = 7; // Navigate to the 7th page (dashboard/scoreboard)
-  dashboardButton.hide(); // Hide the dashboard button
-  console.log("Navigated to Dashboard (Page 7)");
-}
+
 
 function clearData() {
   // Clear all data from localStorage
