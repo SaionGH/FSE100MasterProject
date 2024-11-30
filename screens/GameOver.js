65d526c2e2f22926a750781da05fca95ab368ddb
ANVITAMEKA884 <ameka3@asu.edu>
2024-11-29 22:20:41 -0700

    Update GameOver.js

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -19,6 +19,6 @@ function displayGameOver() {
 
   fill(255);
   textSize(16);
-  text("View Dashboard", width / 2, buttonY + buttonHeight / 2 + 5);
+  text("Click to View Dashboard", width / 2, buttonY + buttonHeight / 2 + 5);
   gameOver = true; // Set gameOver flag
 }
