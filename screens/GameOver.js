5e95fa08e42d6909bf57c783ac2143d9eb2f9c3f
ANVITAMEKA884 <ameka3@asu.edu>
2024-11-29 23:32:44 -0700

    Update GameOver.js

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -8,17 +8,6 @@ function displayGameOver() {
   text("Over!", width / 2, height / 2 - 74);
   textSize(30);
   text("Your Score: " + playerScore, width / 2, height / 2 + 20);
-   // Draw "Click to View Dashboard" button
-  let buttonX1 = width / 2 - 75;
-  let buttonY1 = height / 2 + 50;
-  let buttonWidth1 = 150;
-  let buttonHeight1 = 50;
-
-  fill(200, 50, 50); // Red color
-  rect(buttonX1, buttonY1, buttonWidth1, buttonHeight1, 10); // Rounded corners
-
-  fill(255);
-  textSize(16);
-  text("Click to View Dashboard", width / 2, buttonY + buttonHeight / 2 + 5);
+  text("Click to View Leaderboard", width / 2, height / 2 + 70);
   gameOver = true; // Set gameOver flag
 }
