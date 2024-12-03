67058a0a7663f565485477286587f430af4194b9
ANVITAMEKA884 <ameka3@asu.edu>
2024-12-03 15:50:33 -0700

    Update NameEntries.js

--- screens/NameEntries.js
+++ screens/NameEntries.js
@@ -1,4 +1,4 @@
-/*function displayNameEntry(submitButton) {
+function displayNameEntry(submitButton) {
   fill(0, 0, 0); // Green background for input section
   rect(50, 250, 500, 200); // Background for input section
   fill("white");
@@ -6,4 +6,4 @@
   text("Enter your name:", 200, 280); // Prompt for name
   input.show(); // Show the input field
   submitButton.show(); // Show the submit button
-}*/
+}
