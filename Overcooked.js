c4db69a4e65e090ebd7ad86de5242fd9197ae2b4
ANVITAMEKA884 <ameka3@asu.edu>
2024-12-03 14:45:33 -0700

    Update Overcooked.js

--- Overcooked.js
+++ Overcooked.js
@@ -180,27 +180,13 @@ function checkGameOver() {
 
 function mousePressed() {
   // Check for game over
-if (page === 5) { // Check if the user is on the timeout screen
-  // Add the player's score to the scoreboard if the game is over
   if (gameOver) {
+    if (page === 7) { // Check if game over and in the scoreboard
     if (playerName && playerScore >= 0) {
       scoreboard.push({ name: playerName, score: playerScore });
     }
     saveData(); // Save data before transitioning
     grillSound.play(); // Optional sound effect
-    page = 7; // Navigate to the scoreboard
-    return; // Exit function to avoid other conditions running
-  }// Check if "Click to View Scoreboard" was clicked on the game over or dashboard screens
-  if (
-    page === 5 && // Scoreboard page
-    mouseX >= width / 2 - 75 &&
-    mouseX <= width / 2 + 75 &&
-    mouseY >= height - 100 &&
-    mouseY <= height - 50
-  ) {
-    grillSound.play(); // Optional: Play sound effect
-    page = 7; 
-    console.log("hello");// Navigate to the dashboard page
     return;
   }
     // Other reset actions
