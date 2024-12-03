566759f42e5990c3129ca9847b6cf8fc1e991701
ANVITAMEKA884 <ameka3@asu.edu>
2024-12-03 13:59:38 -0700

    Update Overcooked.js

--- Overcooked.js
+++ Overcooked.js
@@ -64,7 +64,7 @@ function handleViewScoreboard() {
   const confirmTransition = confirm("Are you sure you want to view the scoreboard?");
   if (confirmTransition) {
     grillSound.play(); // Optional sound effect
-     page = 7;// Navigate to the scoreboard
+    page = 7;// Navigate to the scoreboard
   }
 }
 function setup() {
