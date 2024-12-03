7c1baa7a4e3f501271dc296fdd9d7d64802198fe
ANVITAMEKA884 <ameka3@asu.edu>
2024-12-02 20:11:05 -0700

    Update Overcooked.js

--- Overcooked.js
+++ Overcooked.js
@@ -119,7 +119,6 @@ function draw() {
   background(0);
   if (gameOver) {
     displayGameOver();
-    dashboardButton.show(); // Skip other rendering when game is over
   }
   if (page === 0) {
     displayMainMenu();
