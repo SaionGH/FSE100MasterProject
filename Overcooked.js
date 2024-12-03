6b260c13c4e18e662ee04d5b3f4446c043fce093
ANVITAMEKA884 <ameka3@asu.edu>
2024-12-02 21:55:54 -0700

    Update Overcooked.js

--- Overcooked.js
+++ Overcooked.js
@@ -152,7 +152,7 @@ function draw() {
 function checkGameOver() {
   if (playerScore <= 0) {
     gameOver = true; // Set game over status
-    page = 5; // Go to timeout screen
+    return; // Go to timeout screen
   }
 }
 
