4741e1dfad69a6aacc589b7a3326b387047034ea
ANVITAMEKA884 <ameka3@asu.edu>
2024-12-02 21:37:03 -0700

    Update Order.js

--- components/Order.js
+++ components/Order.js
@@ -7,15 +7,6 @@ class Order {
     this.timer = customer.patience;
   }
 
-  function updateTimer() {
-  }
-  // Decreases the timer each frame
-  function displayTimer() {
-  fill(255); // White text
-  textSize(24); // Set text size
-  textAlign(RIGHT, BOTTOM); // Align text to bottom right
-  text(`Time: ${Math.ceil(timer)}s`, width - 20, height - 20); // Position near bottom-right corner
-}
 
   // Checks if the player's dish matches the recipe
   checkCompletion(playerDish) {
