2b4af9801430ae0d9e2a8971b8c363d17eac38a4
ANVITAMEKA884 <ameka3@asu.edu>
2024-12-03 16:40:08 -0700

    Update Overcooked.js

--- Overcooked.js
+++ Overcooked.js
@@ -88,6 +88,7 @@ function setup() {
   viewScoreboardButton.style("padding", "10px");
   viewScoreboardButton.style("border-radius", "5px");
   viewScoreboardButton.mousePressed(handleViewScoreboard);
+   window.viewScoreboardButton = viewScoreboardButton;
 
   // Store the button reference globally for toggling visibility later
   loadData();
