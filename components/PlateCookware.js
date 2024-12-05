5a5cb561ff3533e53d5c40fd03d445b77c4e2d90
SaionGH <111557861+SaionGH@users.noreply.github.com>
2024-12-04 22:24:35 -0700

    Update PlateCookware.js

--- components/PlateCookware.js
+++ components/PlateCookware.js
@@ -12,48 +12,39 @@ class Plate extends Cookware {
     if (this.ingredients.length < this.maxIngredients) {
       this.ingredients.push(ingredient);
       this.arrangeContents();
-      console.log("Ingredient added to plate:", ingredient);
-    } else {
-      console.log("Plate is full! Cannot add more ingredients.");
-    }
+    } 
   }
   
   arrangeContents() {
-        const radius = 15; // Distance from the center of the plate
+        const radius = 15; 
         this.ingredients.forEach((ingredient, index) => {
             const angle = (TWO_PI / this.ingredients.length) * index;
             ingredient.drawX = this.x + cos(angle) * radius;
             ingredient.drawY = this.y + sin(angle) * radius;
         });
-  }
+  } 
   
-  // Overrides processContents to reflect arranging rather than cooking
   processContents() {
-    // Possibly simply calls arrangeContents()
     this.arrangeContents();
   }
 
-  // Draws the plate with its contents
   draw() {
     push();
-    // Draw the plate
     
     fill("#FFFFFF");
-    ellipse(this.x, this.y, 40, 10); // Plate outline
+    ellipse(this.x, this.y, 40, 10); 
     fill("#E0E0E0");
-    ellipse(this.x, this.y, 30, 8); // Inner plate
+    ellipse(this.x, this.y, 30, 8); 
 
-    // Draw ingredients
     this.ingredients.forEach((ingredient) => {
     if (ingredient.image) {
-        image(ingredient.image, ingredient.drawX, ingredient.drawY, 20, 20); // Adjust size
+        image(ingredient.image, ingredient.drawX, ingredient.drawY, 20, 20); 
     } else {
-        console.warn("Ingredient missing image:", ingredient);
+        console.log("Ingredient missing image:", ingredient);
         fill(ingredient.color || "#FF0000");
         ellipse(ingredient.drawX, ingredient.drawY, 10, 10);
     }
 });
-
     pop();
   }
 }
